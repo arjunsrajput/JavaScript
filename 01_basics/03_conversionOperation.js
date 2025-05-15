32bb5cb71ee83dd48696ad0133b2cf1f5c5f9f6b
Arjun Singh Rajput <arjunitofficial@gmail.com>
2025-05-15 11:49:06 +0000

    datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="33"
+console.log(score) //33
+console.log(typeof(score)) //string
+let valueinNumber=Number(score)
+console.log(valueinNumber); //33
+console.log(typeof valueinNumber); //number
+
+let nscore="2223abc"
+console.log(nscore) //2223abc
+let valueNscore=Number(nscore)
+console.log(typeof(valueNscore)) //number
+console.log(valueNscore) //NaN
+
+let isLoggenIn=1
+let booleanIsLoggenIn=Boolean(isLoggenIn);
+console.log(isLoggenIn) //1
+console.log(typeof isLoggenIn) //number
+
+console.log(booleanIsLoggenIn) //true
+console.log(typeof booleanIsLoggenIn) //boolean
+
+
+
+
+
+
